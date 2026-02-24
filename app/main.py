8e976966bcc53d9a66f4f1910e415c6d48df1ed9
Reuben Nogueira <reubs1910@gmail.com>
2026-02-23 22:41:05 -0800

    Added endpoint
    Added new endpoint to get today's matches

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+import httpx
+from app.config import FOOTBALL_API_KEY
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Footy API is alive!"}
+
+
+@app.get("/matches/today")
+async def get_todays_matches():
+    url = "https://api.football-data.org/v4/matches"
+    headers = {"X-Auth-Token" : FOOTBALL_API_KEY}
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        data = response.json()
+
+    return data
