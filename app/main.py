84a9ec3a4156019562f39c50112059dbf1108b75
Reuben Nogueira <reubs1910@gmail.com>
2026-02-24 10:59:25 -0800

    Trimmed data
    Cleaned up data to return only what is needed.

--- app/main.py
+++ app/main.py
@@ -14,8 +14,25 @@ async def get_todays_matches():
     url = "https://api.football-data.org/v4/matches"
     headers = {"X-Auth-Token" : FOOTBALL_API_KEY}
 
+    #retrieves all data
     async with httpx.AsyncClient() as client:
         response = await client.get(url, headers=headers)
         data = response.json()
 
-    return data
+    #extracts just what we need
+    matches = []
+    for match in data.get("matches", []):
+        matches.append({
+            "id": match.get("id"),
+            "time": match.get("utcDate"),
+            "status": match.get("status"),
+            "competition": match.get("competition", {}).get("name"),
+            "home_team": match.get("homeTeam", {}).get("name"),
+            "away_team": match.get("awayTeam", {}).get("name"),
+            "score": {
+                "home": match.get("score", {}).get("fullTime", {}).get("home"),
+                "away": match.get("score", {}).get("fullTime", {}).get("away"),
+            }
+        })
+
+    return {"count": len(matches), "matches": matches}
