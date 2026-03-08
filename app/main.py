4a9a62bf29c09e38fc2c9fec81bd3e832b4ddff6
Reuben Nogueira <reubs1910@gmail.com>
2026-03-07 17:05:38 -0800

    Added enpoint to get league standings
    Using different path parameters, you can recieve the trimmed standings for different leagues.

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 def root():
     return {"message": "Footy API is alive!"}
 
-
+#endpoint to get today's matches
 @app.get("/matches/today")
 async def get_todays_matches():
     url = "https://api.football-data.org/v4/matches"
@@ -35,4 +35,46 @@ async def get_todays_matches():
             }
         })
 
-    return {"count": len(matches), "matches": matches}
+    return {
+        "count": len(matches), 
+        "matches": matches
+    }
+
+#endpoint to get standings from different leagues
+@app.get("/standings/{competition_code}")
+async def get_standings(competition_code: str):
+    url = f"https://api.football-data.org/v4/competitions/{competition_code}/standings"
+    headers = {"X-Auth-Token": FOOTBALL_API_KEY}
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        data = response.json()
+
+    #since multiple tables (home, away, etc), we only want the Total one
+    standings = data.get("standings", [])
+    table = None;
+    for standing in standings:
+        if standing.get("type") == "TOTAL":
+            table = standing.get("table", [])
+
+    cleaned_table = []
+    for row in table:
+        cleaned_table.append({
+            "position": row.get("position"),
+            "team": row.get("team", {}).get("name"),
+            "logo": row.get("team", {}).get("crest"),
+            "gamesPlayed": row.get("playedGames"),
+            "form": row.get("form"),
+            "won": row.get("won"),
+            "draw": row.get("draw"),
+            "lost": row.get("lost"),
+            "points": row.get("points"),
+            "goalsFor": row.get("goalsFor"),
+            "goalsAgainst": row.get("goalsAgainst"),
+            "goalDiff": row.get("goalDifference")
+        })
+
+    return {
+        "league": data.get("competition", {}).get("name"),
+        "table": cleaned_table
+    }
